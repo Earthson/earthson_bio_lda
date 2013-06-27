585005d194bb62b8ef07fa3548cab25d32cdbd60
Earthson Lu <Earthson.Lu@gmail.com>
2013-06-27 22:10:17 +0800

    add kl

--- lda.ml
+++ lda.ml
@@ -241,3 +241,19 @@ let run_array () =
 
 let _ = run_array();;
 (*let _ = run_list();;*)
+
+let doc_dist m =
+    Array.init kkk (fun x -> 
+        ((float_of_int omcnt.(m))+.alpha)/.((float_of_int nm.(m))+.((float_of_int kkk)*.alpha)));;
+
+let word_dist m t =
+    let ans = Array.init kkk (fun x -> prob m t x) in
+    let sum = Array.fold_left (fun x y -> x+.y) in
+    Array.iteri (fun i x -> ans.(i) <- x/.sum) ans;
+    ans;;
+
+let kl_dis px py =
+    let rec for_iter ans i = 
+        if i == Array.length px then ans
+        else for_iter (ans+.px.(i)*.(log px.(i))/.py.(i)/.(log 2.0)) (i+1)
+    in for_iter 0.0 0;;
